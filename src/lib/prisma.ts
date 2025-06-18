d4137928ef9892a6e7265da8f6447878f6006239
SiKcL <bkdaniels8@gmail.com>
2025-06-17 21:31:01 -0400

    Fix Prisma y seccion FAQ

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,15 +2,11 @@
 
 import { PrismaClient } from '@prisma/client';
 
-// Declaramos una variable global para el cliente de Prisma
-// para evitar crear múltiples conexiones en desarrollo.
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-// Si ya existe una instancia de prisma, la usamos.
-// Si no, creamos una nueva.
-// 'globalThis.prisma' no se ve afectado por el Hot Reload de Next.js.
 const prisma = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {

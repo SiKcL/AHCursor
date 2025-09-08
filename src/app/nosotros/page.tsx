ea123e8c3ae85dd3566cc8f71656c6e3a3d9ed8d
SiKcL <bkdaniels8@gmail.com>
2025-09-08 16:20:14 -0300

    Actualizacion de Apartado Nosotros

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -26,14 +26,15 @@ export default function NosotrosPage() {
         <div className="bg-white p-8 rounded-lg shadow-lg border border-gray-200">
             <h2 className="text-3xl font-bold text-gray-900 mb-4">Sobre nosotros</h2>
             <p className="text-gray-700 leading-relaxed">
-              Agrícola Horizonte es una empresa familiar, dedicada al cultivo de hortalizas tanto hidropónicas como tradicionales. Nos enorgullecemos de cultivar productos limpios y de alta calidad, comprometidos con la sostenibilidad y el cuidado del medio ambiente.
+            Somos Agrícola Horizonte Spa, una empresa que se dedica a la producción de hortalizas en la zona rural de Melipilla, Chile.  Si bien nuestra formalización data de 2022, somos herederos de un legado de generaciones dedicadas al campo, con un conocimiento transmitido con profundo respeto por la naturaleza. Entendemos que la agricultura no es solo un medio de producción, sino una forma de vida que debe ser preservada y fortalecida.<br></br><br></br>
+            Con el objetivo de adaptarnos a los desafíos actuales, hemos implementado un enfoque de agricultura sostenible que combina el legado de la agricultura tradicional y agroecológica con las innovaciones modernas de la hidroponía. Esta fusión nos permite maximizar el uso de los espacios disponibles, minimizando el impacto ambiental y garantizando un suministro constante de hortalizas frescas y nutritivas. Nos enorgullece preservar nuestras raíces mientras miramos hacia el futuro, comprometidos con prácticas que benefician tanto a nuestros clientes como al medio ambiente.
             </p>
         </div>
           <br />
         <div className="bg-white p-8 rounded-lg shadow-lg border border-gray-200">
             <h2 className="text-3xl font-bold text-gray-900 mb-4">Nuestra misión</h2>
             <p className="text-gray-700 leading-relaxed">
-              AGRICOLA HORIZONTE, Empresa productora de hortalizas tradicionales y en hidroponia, perteneciente a la comuna de MELIPILLA.
+              En Agrícola Horizonte SPA, nos comprometemos a ser líderes en la producción sostenible de hortalizas, asegurando que cada producto que entregamos no solo cumpla con estándares de calidad, sino que también promueva prácticas que protejan el medio ambiente y mejoren la vida de nuestra comunidad. Buscamos inspirar un cambio positivo en la forma en que se produce y consume la agricultura. Creemos que al respetar y cuidar nuestra tierra, no solo preservamos un legado familiar, sino que también aseguramos un futuro próspero para las próximas generaciones.
             </p>
           </div>
     </div>
